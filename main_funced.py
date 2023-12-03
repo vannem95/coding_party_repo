38f4c68ef631cbf86e8242f12dfddc07a5165c9c
Vivek Annem <va19e@my.fsu.edu>
2023-12-02 19:28:57 -0500

    test - func-ified

--- main_funced.py
+++ main_funced.py
@@ -2,12 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time as tm
 
+def euler_step(state,dt):
+
+	th = state[0]
+	dth = state[1]
+
+	m = 1.0
+	g = 9.8
+	L = 1.0
+
+	ddth = - m * g * L * np.sin(th)
+	dth2 = dth + ddth*dt
+	th2 = th + dth2*dt
+
+	state2 = [th2, dth2]
+	return state2
 
 def main(argv=None):
 	
 	t0 = 0.0
-	tf = 10.0
-	tstep = 0.1
+	tf = 2.0
+	tstep = 0.05
 	time = np.arange(t0 , tf , tstep)
 
 	pendL = 1.0
@@ -16,20 +31,24 @@ def main(argv=None):
 	pendM = 1.0
 	g = 9.8
 
+	lim = 1.5
+
 	for t in time:
 
-		ddTH = -pendM*g*pendL*np.sin(pendTH)
-		dTH += ddTH*tstep
-		pendTH += dTH*tstep
+		# ddTH = -pendM*g*pendL*np.sin(pendTH)
+		# dTH += ddTH*tstep
+		# pendTH += dTH*tstep
+
+		[pendTH,dTH] = euler_step([pendTH,dTH],tstep)
+
 		pendX = pendL*np.sin(pendTH)
 		pendY = -pendL*np.cos(pendTH)
 		plt.cla()
 		plt.plot([0.0,pendX],[0.0,pendY])
-		circle1 = plt.Circle((pendX, pendY), 0.2, color='r')
+		circle1 = plt.Circle((pendX, pendY), 0.1, color='r')
 		plt.gca().add_patch(circle1)
-		plt.axis([-2,2,-2,2])
-		plt.pause(.05)
-
+		plt.axis(lim*np.array([-1,1,-1,1]))
+		plt.pause(.001)
 
 
 
